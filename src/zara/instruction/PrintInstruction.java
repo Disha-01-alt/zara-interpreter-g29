310aebe01cedddcdb0b5705f4a7fc590add14926
Shivam-Shukla0 <shivshukla00846@gmail.com>
2026-03-24 22:14:36 +0530

    feat: updated PrintInstruction execute method

--- src/zara/instruction/PrintInstruction.java
+++ src/zara/instruction/PrintInstruction.java
@@ -3,15 +3,35 @@ package zara.instruction;
 import zara.ast.Expression;
 import zara.runtime.Environment;
 
-public class PrintInstruction implements Instruction {
+public class PrintInstruction implements Instruction
+{
     private final Expression expression;
 
-    public PrintInstruction(Expression expression) {
+    public PrintInstruction(Expression expression)
+    {
         this.expression = expression;
     }
 
     @Override
-    public void execute(Environment env) {
-        // TODO: Execute Print
+    public void execute(Environment env)
+    {
+
+        Object result = expression.evaluate(env);
+        if (result instanceof Double)
+        {
+            double d = (Double) result;
+            if (d == Math.floor(d))
+            {
+                System.out.println((int) d);
+            }
+            else
+            {
+                System.out.println(d);
+            }
+        }
+        else
+        {
+            System.out.println(result);
+        }
     }
 }
