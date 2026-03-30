26ecf88f9d187881c1ce881bc115c2ea8b326df9
Shivam-Shukla0 <shivshukla00846@gmail.com>
2026-03-30 18:05:26 +0530

    test: add unit tests for RepeatInstruction

--- test/java/zara/instruction/RepeatInstructionTest.java
+++ test/java/zara/instruction/RepeatInstructionTest.java
@@ -1,4 +1,61 @@
 package zara.instruction;
 
-public class RepeatInstructionTest {
+import org.junit.jupiter.api.Test;
+import zara.ast.*;
+import zara.runtime.Environment;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RepeatInstructionTest {
+
+    @Test
+    void shouldExecuteBodyCorrectNumberOfTimes() {
+        Environment env = new Environment();
+        env.set("counter", 0.0);
+
+        List<Instruction> body = Arrays.asList(
+                new AssignInstruction("counter",
+                        new BinaryOpNode(new VariableNode("counter"), "+", new NumberNode(1)))
+        );
+
+        RepeatInstruction repeat = new RepeatInstruction(5, body);
+        repeat.execute(env);
+
+        assertEquals(5.0, env.get("counter"));
+    }
+
+    @Test
+    void shouldNotExecuteBodyWhenCountIsZero() {
+        Environment env = new Environment();
+        env.set("counter", 0.0);
+
+        List<Instruction> body = Arrays.asList(
+                new AssignInstruction("counter",
+                        new BinaryOpNode(new VariableNode("counter"), "+", new NumberNode(1)))
+        );
+
+        RepeatInstruction repeat = new RepeatInstruction(0, body);
+        repeat.execute(env);
+
+        assertEquals(0.0, env.get("counter"));
+    }
+
+    @Test
+    void shouldIncrementVariableInLoop() {
+        Environment env = new Environment();
+        env.set("i", 1.0);
+
+        List<Instruction> body = Arrays.asList(
+                new AssignInstruction("i",
+                        new BinaryOpNode(new VariableNode("i"), "+", new NumberNode(1)))
+        );
+
+        RepeatInstruction repeat = new RepeatInstruction(4, body);
+        repeat.execute(env);
+
+        assertEquals(5.0, env.get("i"));
+    }
 }
